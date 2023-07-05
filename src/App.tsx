e365a6cc4dbf0f1655d1f43b0962a39404f0b311
MICHAEL EMMANUEL <mickeystarzy@gmail.com>
2023-07-05 22:52:45 +0100

    cospire-tailwind-installed

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1 className="text-3xl text-red-400 font-bold underline">cospire</h1>
+    </div>
+  );
+};
+
+export default App;
